d3290a63bbbf43036468becb3d9e16168bc49bf6
TapatioSpice <157397198+TapatioSpice@users.noreply.github.com>
2026-09-18 10:45:30 -0700

    Add files via upload

--- 2.0Version.py
+++ 2.0Version.py
@@ -70,6 +70,7 @@ BUILDER_FILES = {
     "Pulte": "PulteContracts1.xlsx",
     "Woodside": "WoodsideContracts1.xlsx",
     "Richmond": "RichmondContracts1.xlsx",
+    "Taylor Morrison": "TaylorMorrisonContracts1.xlsx",
     "KB Homes": "KBHomesContracts1.xlsx",
 }
 
